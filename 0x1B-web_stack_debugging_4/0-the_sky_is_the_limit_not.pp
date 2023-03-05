6ec0e64ba0c7a13d30655285b9f4d0fc28b6694a
godima20 <godima20@gmail.com>
2023-03-05 05:46:03 +0000

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # Fix problem of high amount of requests
 
 exec {'replace':
-   provider => shell,
-   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-   before   => Exec['restart'],
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
 exec {'restart':
