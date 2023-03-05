0dc4d06cf25358898c501084c219e9411828bad7
godima20 <godima20@gmail.com>
2023-03-05 10:00:54 +0000

    fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -16,30 +16,30 @@ def add_title(hot_list, hot_posts):
     add_title(hot_list, hot_posts)
 
 
-    def recurse(subreddit, hot_list=[], after=None):
-        """ Queries to Reddit API """
-        u_agent = 'Mozilla/5.0'
-        headers = {
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
             'User-Agent': u_agent
         }
 
-        params = {
+    params = {
             'after': after
-        }
+    }
 
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        res = requests.get(url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
 
-        if res.status_code != 200:
-            return None
+    if res.status_code != 200:
+        return None
 
-        dic = res.json()
-        hot_posts = dic['data']['children']
-        add_title(hot_list, hot_posts)
-        after = dic['data']['after']
-        if not after:
-            return hot_list
-        return recurse(subreddit, hot_list=hot_list, after=after)
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
