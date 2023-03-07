b8758da083eea1f4191fa8b334140258e8ccd877
godima20 <godima20@gmail.com>
2023-03-07 15:11:50 +0000

    Fix

--- 0x16-api_advanced/2-main.py
+++ 0x16-api_advanced/2-main.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-         result = recurse(sys.argv[1])
-         if result is not None:
-             print(len(result))
-         else:
-             print("None")
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
