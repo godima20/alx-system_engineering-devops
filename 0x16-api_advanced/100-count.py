baf2e068a3b0bbf21bea104db57a5aa1915bd6b1
godima20 <godima20@gmail.com>
2023-03-09 21:27:34 +0000

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -2,18 +2,29 @@
 """returns the number of subscribers (not active users,
 total subscribers) for a given subreddit"""
 
-import requests
-import sys
-after = None
-count_dict = []
+import requests as r
+from sys import argv
 
 
-def count_words(subreddit, word_list):
-    """returns the number of subscribers"""
-    global after
-    global count_dict
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Spam4Karma'}
-    param = {'after': after}
-    res = requests.get(url, headers=headers, allow_redirects=False,
-                       params=param)
+def count_words(subreddit, word_list, key=""):
+    """
+    recursive function that queries the Reddit API,
+    parses the title of all hot articles, and prints
+    a sorted count of given keywords (case-insensitive,
+    delimited by spaces. Javascript should count as
+    javascript, but java should not).
+    """
+    headers = {"User-Agent": "Frocuts"}
+    endpoint = "http://reddit.com/r/{}/hot.json?after={}"
+    subs = r.get(endpoint.format(subreddit, key), headers=headers)
+    if subs.status_code != 200:
+        print(None)
+        return 0
+    subs = subs.json()
+    if subs["kind"] == "Listing":
+        # for data in subs["data"]["children"]:
+        # print(data["data"]["title"])
+        for word in word_list:
+            print('{}: {}'.format(word, 0))
+        else:
+            print(None)
